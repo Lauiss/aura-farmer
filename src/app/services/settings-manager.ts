92c8fe91729ab38f6cb987d253005938fb2d1cdb
Florian A. <67206134+Lauiss@users.noreply.github.com>
2025-11-25 10:09:55 +0100

    Update settings-manager.ts

--- src/app/services/settings-manager.ts
+++ src/app/services/settings-manager.ts
@@ -24,7 +24,7 @@ export class SettingsManager {
     language: 'en'
   };
 
-  settingConfig;
+  settingConfig: SettingsConfig = this.defaultConfig;
 
   constructor() {
     this.loadSettingsConfig();
